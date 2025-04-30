768b55f23ae1e107d0cb25184b6f958d6efe731a
Vladimir Hrechko <grechko_vo@navis.ru>
2025-04-30 13:26:43 +0300

    Добавил проверку на пустые поля

--- src/main/java/sf/mifi/grechko/dto/UserDto.java
+++ src/main/java/sf/mifi/grechko/dto/UserDto.java
@@ -1,15 +1,20 @@
 package sf.mifi.grechko.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record UserDto(
     Long     id,
+    @NotBlank(message = "Login is empty")
     String   login,
+    @NotBlank(message = "Password is empty")
     String   passwd,
     UserRole role,
+    @NotBlank(message = "Telegram is empty")
     String   telegram,
+    @NotBlank(message = "Email is empty")
     String   email,
     @Pattern(regexp = "^\\+(?:\\d[- ]?){6,14}\\d$", message = "Wrong number format.")
     String   phone
