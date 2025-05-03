c2bba6e7c499c8bcc160b74508c855f854c14d3d
BadLagger <lanynamer1985@gmail.com>
2025-05-04 00:32:55 +0300

    Создал пользователя для отладки

--- src/main/resources/db/migration/V2__insert.sql
+++ src/main/resources/db/migration/V2__insert.sql
@@ -1,2 +1,4 @@
 -- V2__Insert_data.sql
 INSERT INTO OtpConfig (lifetime, sign_number) VALUES (2, 6);
+INSERT INTO Users (login, passwd, role, telegram, email, phone) VALUES
+('badlagger', '$2a$10$mC6iwNKWc/CwtUIMIrTldOczdnmDxJGCMinwED/3E9x1aH3KCX6f.', 'USER', 'badlagger', 'badlagger@yandex.ru', '+79164497400');
